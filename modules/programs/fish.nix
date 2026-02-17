ad5915e41adcdc0ec45d4e895f74a537dc8dac6f
guno327 <accounts@ghov.net>
2026-02-17 17:47:53 -0500

    made gpg-agent pinentry dynamic

--- modules/programs/fish.nix
+++ modules/programs/fish.nix
@@ -11,6 +11,12 @@ in {
     programs.fish = {
       enable = true;
       loginShellInit = ''
+        if set -q SSH_CLIENT || set -q SSH_TTY
+          set -x PINENTRY_USER_DATA "USE_CURSES"
+        else
+          set -x PINENTRY_USER_DATA "USE_GNOME2"
+        end
+
         set -x NIX_PATH nixpkgs=channel:nixos-unstable
         set -x NIX_LOG info
         direnv hook fish | source
--- modules/services/gpg.nix
+++ modules/services/gpg.nix
@@ -6,6 +6,32 @@
 }:
 with lib; let
   cfg = config.modules.services.gpg;
+  pinentry = pkgs.writeScript "pinentry-auto" ''
+    #!/usr/bin/env fish
+
+    # Determine the base pinentry program
+    set pe ${pkgs.pinentry-gnome3}
+
+    # Select the appropriate pinentry based on PINENTRY_USER_DATA
+    switch "$PINENTRY_USER_DATA"
+        case "*USE_TTY*"
+            set pe ${pkgs.pinentry-tty}/bin/pinentry-tty
+        case "*USE_CURSES*"
+            set pe ${pkgs.pinentry-curses}/bin/pinentry-curses
+        case "*USE_GTK2*"
+            set pe ${pkgs.pinentry-gtk2}/bin/pinentry-gtk2
+        case "*USE_GNOME3*"
+            set pe ${pkgs.pinentry-gnome3}/bin/pinentry-gnome3
+        case "*USE_QT*"
+            set pe ${pkgs.pinentry-qt}/bin/pinentry-qt
+        case "*"
+            set pe ${pkgs.pinentry-gnome3}/bin/pinentry-gnome3
+    end
+
+    # Execute the selected pinentry with all arguments
+    exec $pe $argv
+
+  '';
 in {
   options.modules.services.gpg.enable = mkEnableOption "enable extended gpg configuration";
   config = mkIf cfg.enable {
@@ -18,7 +44,7 @@ in {
       enable = true;
       enableFishIntegration = true;
       enableSshSupport = true;
-      pinentry.package = pkgs.pinentry-gnome3;
+      pinentry.package = pinentry;
     };
   };
 }
