36f555a772f30e99a3926c22e28cc56778a6b6a0
guno327 <accounts@ghov.net>
2026-05-01 17:29:35 -0400

    updated ssh match blocks

--- modules/programs/ssh.nix
+++ modules/programs/ssh.nix
@@ -3,18 +3,34 @@
   lib,
   ...
 }:
-with lib; let
+with lib;
+let
   cfg = config.modules.programs.ssh;
-in {
+in
+{
   options.modules.programs.ssh.enable = mkEnableOption "Enable and configure ssh";
   config = mkIf cfg.enable {
     programs.ssh = {
       enable = true;
       enableDefaultConfig = false;
       matchBlocks = {
-        vps = {
-          host = "vps";
+        "*" = {
+          host = "*";
+          setEnv = {
+            TERM = "xterm-256color";
+          };
+        };
+        lighthouse = {
+          host = "lighthouse";
           hostname = "100.100.0.1";
+        };
+        jumphost = {
+          host = "jumphost";
+          hostname = "100.100.0.10";
+        };
+        printer = {
+          host = "printer";
+          hostname = "100.100.0.8";
         };
         server = {
           host = "server";
